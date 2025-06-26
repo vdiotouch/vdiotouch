eb39eabf11cb2fd19b4471ad0cce9fd2369419a9
Toufiq <toufiq.austcse@gmail.com>
2025-06-27 02:18:59 +0600

    fix: remove RABBIT_MQ_THUMBNAIL_GENERATION_ROUTING_KEY from env

--- server/api/src/common/app-config/service/app-config.service.ts
+++ server/api/src/common/app-config/service/app-config.service.ts
@@ -50,9 +50,6 @@ export class AppConfigService {
       RABBIT_MQ_UPDATE_FILE_STATUS_QUEUE: this.configService.getOrThrow('RABBIT_MQ_UPDATE_FILE_STATUS_QUEUE'),
       TEMP_VIDEO_DIRECTORY: this.configService.getOrThrow('TEMP_VIDEO_DIRECTORY'),
       CDN_BASE_URL: this.configService.getOrThrow('CDN_BASE_URL'),
-      RABBIT_MQ_THUMBNAIL_GENERATION_ROUTING_KEY: this.configService.getOrThrow(
-        'RABBIT_MQ_THUMBNAIL_GENERATION_ROUTING_KEY'
-      ),
       DEFAULT_THUMBNAIL_URL: this.configService.getOrThrow('DEFAULT_THUMBNAIL_URL'),
       JWT_EXPIRATION_TIME_IN_SEC: +this.configService.getOrThrow('JWT_EXPIRATION_TIME_IN_SEC'),
       JWT_SECRET: this.configService.getOrThrow('JWT_SECRET'),
