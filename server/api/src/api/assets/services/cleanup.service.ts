87a50d70cbfc7ebee3cc3cfca274bddf7bc2e50a
Toufiq <toufiq.austcse@gmail.com>
2025-07-12 18:28:34 +0600

    fix: cleanup job

--- server/api/src/api/assets/services/cleanup.service.ts
+++ server/api/src/api/assets/services/cleanup.service.ts
@@ -3,10 +3,12 @@ import { FileRepository } from '@/src/api/assets/repositories/file.repository';
 import { Constants, Utils } from 'video-touch-common';
 import fs from 'fs';
 import { AppConfigService } from '@/src/common/app-config/service/app-config.service';
+import mongoose from 'mongoose';
+import { AssetRepository } from '@/src/api/assets/repositories/asset.repository';
 
 @Injectable()
 export class CleanupService {
-  constructor(private fileRepository: FileRepository) {}
+  constructor(private fileRepository: FileRepository, private assetRepository: AssetRepository) {}
 
   async cleanupDevice() {
     const activeDirectories = await this.fileRepository.findAssetIdsWithStatuses([
@@ -16,8 +18,19 @@ export class CleanupService {
     ]);
 
     let allDirectories = fs.readdirSync(AppConfigService.appConfig.TEMP_VIDEO_DIRECTORY, { withFileTypes: true });
+    let dirIds = allDirectories.map((dir) => new mongoose.Types._ObjectId(dir.name));
+    let assets = await this.assetRepository.find({
+      _id: { $in: dirIds },
+    });
+
     for (const dir of allDirectories) {
-      if (dir.isDirectory() && !activeDirectories.includes(dir.name)) {
+      let asset = assets.find((a) => a._id.toString() === dir.name);
+
+      if (
+        dir.isDirectory() &&
+        !activeDirectories.includes(dir.name) &&
+        asset.latest_status === Constants.VIDEO_STATUS.READY
+      ) {
         let localPath = Utils.getLocalVideoRootPath(dir.name, AppConfigService.appConfig.TEMP_VIDEO_DIRECTORY);
         console.log('deleting local path ', localPath);
         if (fs.existsSync(localPath)) {
